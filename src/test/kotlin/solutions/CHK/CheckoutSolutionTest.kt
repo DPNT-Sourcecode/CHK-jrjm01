c86c4c3440f6ac1aebc36961bd8ef6152dfa33fd
sbx_user1051 <sbx_user1051@169.254.6.42>
2024-12-02 21:27:24 +0000

    Mon Dec 02 21:27:24 UTC 2024

--- src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
+++ src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
@@ -7,5 +7,22 @@ class CheckoutSolutionTest {
     @Test
     fun `working test`() {
         assertEquals(260, CheckoutSolution.checkout("A, B, A, B, A, A, C, D"))
+        assertEquals(260, CheckoutSolution.checkout("A B A B A A C D"))
+        assertEquals(260, CheckoutSolution.checkout("A_B_A_B_A_A_C_D"))
+        assertEquals(260, CheckoutSolution.checkout("""
+            A
+            B
+            A
+            B
+            A
+            A
+            C
+            D
+        """.trimIndent()))
+    }
+
+    @Test
+    fun `broken call`() {
+        assertEquals(-1, CheckoutSolution.checkout("A, B, D, C, FFF"))
     }
 }
