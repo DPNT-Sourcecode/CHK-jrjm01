972c9b2c08874d3778eb6d2980c7bcf6671bbd71
sbx_user1051 <sbx_user1051@169.254.6.42>
2024-12-02 20:51:54 +0000

    Mon Dec 02 20:51:54 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -5,8 +5,9 @@ object CheckoutSolution {
     private val priceMapIndividual = mapOf("A" to 50, "B" to 30, "C" to 20, "D" to 15)
     private val specialOffers = mapOf("A" to Pair(3, 130), "B" to Pair(2, 45))
     fun checkout(skus: String): Int {
-        val items = skus.split(",_- ").groupingBy { it }.eachCount()
+        val items = skus.split(Regex.fromLiteral(",_- ")).groupingBy { it }.eachCount()
         println(items)
         return -1
     }
 }
+
