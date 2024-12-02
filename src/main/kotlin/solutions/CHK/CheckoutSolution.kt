5efa095180b81406198359b9ae808b77f3cdfc14
sbx_user1051 <sbx_user1051@169.254.6.42>
2024-12-02 20:42:54 +0000

    Mon Dec 02 20:42:54 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -5,6 +5,7 @@ object CheckoutSolution {
     private val priceMapIndividual = mapOf("A" to 50, "B" to 30, "C" to 20, "D" to 15)
     private val specialOffers = mapOf("A" to Pair(3, 130), "B" to Pair(2, 45))
     fun checkout(skus: String): Int {
+        val items = skus.split(",_- ").grou
         TODO("Solution not implemented")
     }
 }
